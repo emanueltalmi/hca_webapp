0bb8910db421b898e066de5604aa897bfb6b9214
emanueltalmi <etalmi11@gmail.com>
2021-04-26 10:47:58 -0600

    ignore

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# Replace the existing home function with the one below
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# New functions
+@app.route("/mealprep/")
+def mealprep():
+    return render_template("mealprep.html")
+
+@app.route("/groceryorder/")
+def groceryorder():
+    return render_template("groceryorder.html")
+
+@app.route("/medicalorder/")
+def medicalorder():
+    return render_template("medicalorder.html")
+
+@app.route("/login/")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup/")
+def signup():
+    return render_template("signup.html")
